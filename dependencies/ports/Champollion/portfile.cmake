e10618a47f18dc1336b8188e3298d32b14367726
nikitalita <69168929+nikitalita@users.noreply.github.com>
2022-12-26 10:26:45 -0800

    update champollion vcpkg port

--- dependencies/ports/Champollion/portfile.cmake
+++ dependencies/ports/Champollion/portfile.cmake
@@ -1,55 +1,20 @@
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   CURRENT_INSTALLED_DIR     = ${VCPKG_ROOT_DIR}\installed\${TRIPLET}
-#   DOWNLOADS                 = ${VCPKG_ROOT_DIR}\downloads
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#   VCPKG_TOOLCHAIN           = ON OFF
-#   TRIPLET_SYSTEM_ARCH       = arm x86 x64
-#   BUILD_ARCH                = "Win32" "x64" "ARM"
-#   MSBUILD_PLATFORM          = "Win32"/"x64"/${TRIPLET_SYSTEM_ARCH}
-#   DEBUG_CONFIG              = "Debug Static" "Debug Dll"
-#   RELEASE_CONFIG            = "Release Static"" "Release DLL"
-#   VCPKG_TARGET_IS_WINDOWS
-#   VCPKG_TARGET_IS_UWP
-#   VCPKG_TARGET_IS_LINUX
-#   VCPKG_TARGET_IS_OSX
-#   VCPKG_TARGET_IS_FREEBSD
-#   VCPKG_TARGET_IS_ANDROID
-#   VCPKG_TARGET_IS_MINGW
-#   VCPKG_TARGET_EXECUTABLE_SUFFIX
-#   VCPKG_TARGET_STATIC_LIBRARY_SUFFIX
-#   VCPKG_TARGET_SHARED_LIBRARY_SUFFIX
-#
-# 	See additional helpful variables in /docs/maintainers/vcpkg_common_definitions.md
-
-
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO nikitalita/Champollion
-  REF f72bd6a41a6ab32702c677670702640bc8984f37
-  SHA512 96cc234483f3947afc39a4b930c47ec94cb227d7046758ebf93dc17b14de80ebe6a1646a63c4a8b820aa38f6fbf0f0a8b82c686f0f3d74c0a364cce92c0f22d0
-  HEAD_REF c++20
+  REPO Orvid/Champollion
+  REF 94b0e4db7f5f279a969da98e89ef79739fd4ac19
+  SHA512 101f9d0d7171fb4d6f4ceb4221dfc55dda2442020762d189f29fa20194ac2b1c421331e151c7eef3f9e40055aba51d6c7976df639b2d5372170c156f85062ffd
+  HEAD_REF master
 )
 # Check if one or more features are a part of a package installation.
 # See /docs/maintainers/vcpkg_check_features.md for more details
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-  FEATURES # <- Keyword FEATURES is required because INVERTED_FEATURES are being used
-    standalone   CHAMPOLLION_STANDALONE
   INVERTED_FEATURES
 	  standalone   CHAMPOLLION_STATIC_LIBRARY
 )
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 		OPTIONS
-    -DCHAMPOLLION_STATIC_LIBRARY=1
+      ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
